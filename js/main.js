5c804b6bec814fc5346da86250230c436ccde8f0
Thomas <thomas.auffroy@gmail.com>
2021-05-17 18:41:24 +0200

    Write function to display properly images

--- js/main.js
+++ js/main.js
@@ -1,34 +1,61 @@
 function next_fct(event){
     if (event.key === "ArrowRight" || event.key === undefined){
-      counter++;
+        counter++;
         update(counter);;
     }
   }
-  function previous_fct(event){
+function previous_fct(event){
     if (event.key === "ArrowLeft" || event.key === undefined){
-      counter--;
-          update(counter);
+        counter--;
+        update(counter);
     }
-  }
+}
   
-  function update(counter){
-    articles[counter % 3].style.display = "flex";
-    articles[(counter+1) % 3].style.display = "none";
-    articles[(counter+2) % 3].style.display = "none";
+function update(target){
+    let device;
+    if (window.innerWidth > 380)
+        device = "desktop";
+    else
+        device = "mobile";
+
+    if (target < 0)
+        target = (3 + target % 3) % 3;
+    target = Math.abs(target);
+    articles[target % 3].style.display = "flex";
+    articles[(target+1) % 3].style.display = "none";
+    articles[(target+2) % 3].style.display = "none";
     
-    let str = (counter%3 + 1).toString();
-    let bg__config = 'url("images/desktop-image-hero-'+str+'.jpg")';
+    let str = (target%3 + 1).toString();
+    let bg__config = 'url("images/'+device+'-image-hero-'+str+'.jpg")';
    
     header.style.backgroundImage = bg__config;
-  }
+}
+function underline(event){
+    name_ = event.target.innerHTML;
   
-  var buttons = document.querySelectorAll(".slider");
-  var articles = document.querySelectorAll("article");
-  var header = document.querySelector("header");
-  var counter = 0;
+    let len = name_.length;
+    event.target.innerHTML = name_.substring(0, 1) + "<u>" + name_.substring(1, len - 1) + "</u>" + name_.substring(len - 1);
+}
   
-  buttons[0].addEventListener("click", next_fct, false);
-  document.addEventListener("keyup", next_fct, false);
+function reset(event){
+    event.target.innerHTML = name_;
+}
   
-  buttons[1].addEventListener("click", previous_fct, false);
-  document.addEventListener("keyup", previous_fct, false);
+var buttons = document.querySelectorAll(".slider");
+var articles = document.querySelectorAll("article");
+var header = document.querySelector("header");
+var nav__text = document.querySelectorAll("li");
+var name_;
+var counter = 0;
+
+buttons[0].addEventListener("click", next_fct, false);
+document.addEventListener("keyup", next_fct, false);
+  
+buttons[1].addEventListener("click", previous_fct, false);
+document.addEventListener("keyup", previous_fct, false);
+
+
+nav__text.forEach(el => el.firstChild.addEventListener("mouseenter", underline, false));
+nav__text.forEach(el => el.firstChild.addEventListener("mouseleave", reset, false));
+
+update(counter);
